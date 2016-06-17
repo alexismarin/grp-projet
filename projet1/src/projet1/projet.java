d3388fb96d702437b2bc6af3340197debba8592c
Alexis Marin <Hugo@DESKTOP-89E517F.evollis.lan>
2016-06-17 11:49:28 +0200

    errte

--- projet1/src/projet1/projet.java
+++ projet1/src/projet1/projet.java
@@ -4,7 +4,7 @@ public class projet {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("tototestertretretertreter");
+		System.out.println("tototestertrfetreetretertreter");
 	}
 
 }
