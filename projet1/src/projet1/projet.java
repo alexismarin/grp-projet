e41ac434303725383e07b0bc234302e0defd98e5
Alexis Marin <Hugo@DESKTOP-89E517F.evollis.lan>
2016-06-27 15:09:02 +0200

    ezrzr

--- projet1/src/projet1/projet.java
+++ projet1/src/projet1/projet.java
@@ -4,7 +4,7 @@ public class projet {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("tserzetrdffffffjsdhfksttthfdfhsqdfhqdertreterterrtrfdgdgffetreetretertreter");
+		System.out.println("test2ttthfdfhsqdfhqdertreterterrtrfdgdgffetreetretertreter");
 	}
 
 }
